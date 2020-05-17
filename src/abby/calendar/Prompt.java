09e5b3d30667757779fab35ad67f543225db19e4
mac <mac@macui-MacBookAir.local>
2020-05-17 18:42:05 +0900

    Seperate the shell feature in Prompt.java and move the static main to Prompt.java

---
+++ src/abby/calendar/Prompt.java
@@ -0,0 +1,40 @@
+package abby.calendar;
+
+import java.util.Scanner;
+
+public class Prompt {
+	
+	private final static String PROMPT = "cal > ";
+	
+	public void runPrompt() {
+		
+		Scanner scanner = new Scanner(System.in);
+		Calendar cal = new Calendar();
+		
+		int month = 1; // we need to initialize the month to compare in the while loop
+		
+		while (true) {
+		
+			System.out.print(PROMPT);
+			month = scanner.nextInt();	
+			
+			if(month == -1) { break; }
+			if(month > 12) { continue; }
+			
+			System.out.printf("The maximum dates of the month %d is %d \n", month, cal.getMaxdaysOfMonth(month));
+
+		}
+
+		System.out.println("Bye~");
+		scanner.close();
+	}
+	
+	public static void main(String[] args) {
+		
+		// Shell execute
+		Prompt p = new Prompt();
+		p.runPrompt();
+		
+	}
+
+}
